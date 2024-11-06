e0ba8f555e83ac07a07b5c51bbd9288c1a0bfecc
배인호 <ih.bae@baeinhoui-MacBookPro.local>
2024-11-06 18:25:44 +0900

    [Feat] UpgradeZone Trigger

--- Assets/Scripts/UI/UI_BuildingEnhancement.cs
+++ Assets/Scripts/UI/UI_BuildingEnhancement.cs
@@ -1,12 +1,23 @@
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace UI
 {
-    public class UI_BuildingEnhancement : MonoBehaviour
+    public interface IUI_BuildingEnhancement : IInitializable
+    {
+        
+    }
+    
+    public class UI_BuildingEnhancement : MonoBehaviour, IUI_BuildingEnhancement
     {
         [Header("=== 패널 타이틀 ===")]
         [SerializeField] private TextMeshProUGUI Text_popUpTitle;
         [SerializeField] private TextMeshProUGUI Text_buildingLevel;
+        
+        public void Initialize()
+        {
+            Debug.Log("UI_BuildingEnhancement.Initialize");
+        }
     }
 }
--- Assets/Scripts/Units/Stages/Units/Zones/Units/BuildingZones/Modules/UpgradeZones/UpgradeZone.cs
+++ Assets/Scripts/Units/Stages/Units/Zones/Units/BuildingZones/Modules/UpgradeZones/UpgradeZone.cs
@@ -1,14 +1,35 @@
+using System;
+using AmplifyShaderEditor;
 using UnityEngine;
 
 namespace Units.Stages.Units.Zones.Units.BuildingZones.Modules.UpgradeZones
 {
     public interface IUpgradeZone
     {
-        
+        public event Action<bool> OnPlayerConnected;
     }
     
     public class UpgradeZone : MonoBehaviour, IUpgradeZone
     {
+        public event Action<bool> OnPlayerConnected;
+
+        private const string unitLayer = "Unit";
+        private const string playerTag = "Player";
+
+        public void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.layer == LayerMask.NameToLayer(unitLayer) && other.gameObject.CompareTag(playerTag))
+            {
+                OnPlayerConnected?.Invoke(true);
+            }
+        }
         
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.gameObject.layer == LayerMask.NameToLayer(unitLayer) && other.gameObject.CompareTag(playerTag))
+            {
+                OnPlayerConnected?.Invoke(false);
+            }
+        }
     }
 }
