6f059149d11216385dab6e4d5dbdf32fd5d00a48
배인호 <ih.bae@baeinhoui-MacBookPro.local>
2024-11-07 20:38:31 +0900

    [Feat] 예람 CYR_Workspace MainScene 수정

--- Assets/Scripts/Units/Stages/Controllers/VillageZoneController.cs
+++ Assets/Scripts/Units/Stages/Controllers/VillageZoneController.cs
@@ -14,9 +14,7 @@ using Random = System.Random;
 
 namespace Units.Stages.Controllers
 {
-    public interface IVillageZoneController :
-        IRegisterReference<ICreatureController, IBuildingController, IHuntingZoneController, StageCustomSettings,
-            List<EMaterialType>>, IInitializable
+    public interface IVillageZoneController : IRegisterReference<ICreatureController, IBuildingController, IHuntingZoneController, StageCustomSettings, List<EMaterialType>>, IInitializable
     {
         public IPlayer Player { get; }
         public Action<IPlayer, bool> OnRegisterPlayer { get; set; }
