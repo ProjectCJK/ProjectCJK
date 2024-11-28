25a9ad83f898bdeeac451d510f39e769ca9b7220
배인호 <ih.bae@baeinhoui-MacBookPro.local>
2024-11-28 16:42:33 +0900

    1

--- Assets/Scripts/Interfaces/IRegisterReference.cs
+++ Assets/Scripts/Interfaces/IRegisterReference.cs
@@ -32,13 +32,11 @@ namespace Interfaces
 
     public interface IRegisterReference<in T1, in T2, in T3, in T4, in T5, in T6>
     {
-        public void RegisterReference(T1 instance1, T2 instance2, T3 instance3, T4 instance4, T5 instance5,
-            T6 instance6);
+        public void RegisterReference(T1 instance1, T2 instance2, T3 instance3, T4 instance4, T5 instance5, T6 instance6);
     }
 
     public interface IRegisterReference<in T1, in T2, in T3, in T4, in T5, in T6, in T7>
     {
-        public void RegisterReference(T1 instance1, T2 instance2, T3 instance3, T4 instance4, T5 instance5,
-            T6 instance6, T7 instance7);
+        public void RegisterReference(T1 instance1, T2 instance2, T3 instance3, T4 instance4, T5 instance5, T6 instance6, T7 instance7);
     }
 }
